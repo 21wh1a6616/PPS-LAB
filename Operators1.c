2d4c2455f28e37ddd0dd24d1791b7c764c44c2be
21wh1a6616 <96616246+21wh1a6616@users.noreply.github.com>
2022-01-05 20:45:28 +0530

    Add files via upload

---
+++ Operators1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the values of operands a and b");
+	scanf("%d %d",&a,&b);
+	printf("Arithmatic operations %d %d %d %d %d\n",a+b,a-b,a*b,a/b,a%b);
+	printf("Relational operations %d %d %d %d\n",a<b,a>b,a<=b,a>=b);
+	printf("logical operations %d %d %d\n",a&&b,a||b,!(a=b));
+	
+
+}
