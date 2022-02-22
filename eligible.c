4a76c5822aaf13b83906a6f147fc0bb7e1ca9a8f
21wh1a6616 <96616246+21wh1a6616@users.noreply.github.com>
2022-02-22 16:15:43 +0530

    Add files via upload

---
+++ eligible.c
@@ -0,0 +1,15 @@
+//find the eligibility to vote
+#include<stdio.h>
+int main()
+
+{
+	int age;
+	scanf("%d",&age);
+	if(age>=18)
+	{
+		printf("eligible");
+	}else
+	{
+		printf("Not eligible");
+	}
+}
