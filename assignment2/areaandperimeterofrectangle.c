b2c90b136493731ece40cb62f35a93be49894d7c
21wh1a6616 <96616246+21wh1a6616@users.noreply.github.com>
2022-03-29 17:33:12 +0530

    Update areaandperimeterofrectangle.c

--- assignment2/areaandperimeterofrectangle.c
+++ assignment2/areaandperimeterofrectangle.c
@@ -7,6 +7,6 @@ int main()
 	printf("length amd breadth of recatangle are ");
 	scanf("%d%d",&l,&b);
 	area=l*b;
-	perimeter=(l+b)/2;
+	perimeter=(l+b)*2;
 	printf("area=%d and perimeter=%d",area,perimeter);
 }
